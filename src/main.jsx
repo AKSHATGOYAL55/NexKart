a97f33bb727ccfc9d29a58e81db196e8f0f900fd
Akshat Goyal <akshatg525@gmail.com>
2026-08-03 09:07:22 +0530

    fix bugs in UI

--- src/main.jsx
+++ src/main.jsx
@@ -43,7 +43,7 @@ createRoot(document.getElementById('root')).render(
         <Toaster
           position="top-right"
           toastOptions={{
-            duration: 3000,
+            duration: 2000,
             style: {
               background: '#363636',
               color: '#fff',
